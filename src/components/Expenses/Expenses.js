5ed2e0e471cbc73683e94b8c41f28daed71ba43b
Ariel <arielmalagutti.job@gmail.com>
2022-07-01 15:26:41 -0300

    feat: create state with selected year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,22 +2,34 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 function Expenses({ expenses }) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      {expenses.map((items) => {
-        return (
-          <>
-            <ExpenseItem
-              title={items.title}
-              amount={items.amount}
-              date={items.date}
-            />
-          </>
-        );
-      })}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expenses.map((items) => {
+          return (
+            <>
+              <ExpenseItem
+                title={items.title}
+                amount={items.amount}
+                date={items.date}
+              />
+            </>
+          );
+        })}
+      </Card>
+    </div>
   );
 }
 
